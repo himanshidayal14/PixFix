35a73e24ed9b4bb013cf846362991120e78bf83b
himanshidayal14 <himansheedayal@gmail.com>
2024-09-10 16:43:28 +0530

    js file updated

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
     const fileinput = document.querySelector(".file-input"),
     filteroptions = document.querySelectorAll(".filter button"),
     filtername = document.querySelector(".filter-info .name"),
-    filtervalue =document.querySelector(".filter-info .value")
+    filtervalue =document.querySelector(".filter-info .value"),
     filterslider = document.querySelector(".slider input"),
-    rotateoption= document.querySelector(".rotate button")
+    rotateoption= document.querySelectorAll(".rotate button"),
     previewimg = document.querySelector(".preview-img img"),
-    chooseimgbtn = document.querySelector(".choose-img");
+    resetfilterbtn = document.querySelector(".reset-filter"),
+    chooseimgbtn = document.querySelector(".choose-img"),
+    saveimgbtn = document.querySelector(".save-img");
+
 
 let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
+let rotate = 0, fliphorizontal = 1, flipvertical = 1;
 
 const applyfilter = () => {
+    previewimg.style.transform = `rotate(${rotate}deg) scale(${flipvertical},${fliphorizontal})`;
     previewimg.style.filter=`brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%) `
 }
 
@@ -66,12 +71,37 @@ const updatefilter = () =>
     applyfilter();
 }
 
-rotateoption.forEach(option => {
+ rotateoption.forEach(option => {
     option.addEventListener("click", () => { //adding click event listener to all rotate/flip buttons 
-        
-    })
-});
+        if (option.id == "left") {
+            rotate -= 90; //if clicked btn is left rotate,decrement rotate value by -90
+        } else if (option.id === "right") {
+           rotate += 90; //if clicked btn is right rotate,increment rotate value by +90 
+        } else if (option.id === "horizontal") {
+            //if fliphorizontal value is 1,set this value to -1 else set 1
+              fliphorizontal = fliphorizontal === 1 ? -1 : 1;
+        } else if(option.id==="vertical") {
+            //if flipvertical value is 1 , set this value to -1 else set 1
+            flipvertical = flipvertical === 1 ? -1 : 1;
+        }
+        applyfilter();
+    });
+ }); 
+
+const resetfilter = () => { //reset all value to previous ones 
+    brightness = 100; saturation = 100; inversion = 0; grayscale = 0;
+    rotate = 0; fliphorizontal = 1; flipvertical = 1;
+    rotate = 0; fliphorizontal = 1; flipvertical = 1;
+    filteroptions[0].click();//clicking brightness btn,so that the brightness selected by default
+    applyfilter();
+}
+ 
+const saveimg=() => {
+    
+}
 
 fileinput.addEventListener("change", loadimage);
 filterslider.addEventListener("input", updatefilter);
+resetfilterbtn.addEventListener("click", resetfilter);
+saveimgbtn.addEventListener("click", saveimg);
 chooseimgbtn.addEventListener("click", () => fileinput.click());
